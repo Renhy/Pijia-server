dede76b738ab658ba508015c157cc37b0bff1496
unknown <592672378@qq.com>
2018-02-14 00:43:49 +0800

    [move] backend files to /

--- src/main/java/com/renhy/home/config/MybatisConfig.java
+++ src/main/java/com/renhy/home/config/MybatisConfig.java
@@ -1,11 +1,15 @@
 package com.renhy.home.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 /**
  * Mybatis Configuration
@@ -20,12 +24,20 @@ public class MybatisConfig {
     public SqlSessionFactory sqlSessionFactory() throws Exception {
 
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         sqlSessionFactoryBean.setMapperLocations(resolver.getResources(MAPPER_FILE_PATH));
         sqlSessionFactoryBean.setConfigLocation(resolver.getResource(MYBATIS_CONFIG_PATH));
         sqlSessionFactoryBean.setVfs(SpringBootVFS.class);
 
         return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new DruidDataSource();
     }
 
 }
